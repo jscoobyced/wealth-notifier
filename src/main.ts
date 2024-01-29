ddb6ec32c26c5dd6f94b8bb0e0c2cd3a4a691a7a
Cédric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2024-01-29 22:19:42 +0700

    Check multiple exchange rates.

--- src/main.ts
+++ src/main.ts
@@ -9,25 +9,46 @@ import {
 import SlackService from './slack'
 
 export const run = async () => {
-  const marketDataService = new MarketDataService()
   const ratio = OUNCE_TO_GRAM / GRAM_PER_BAHT
   const request: MarketDataRequest = {
     ratio,
     currency: 'XAUTHB',
   }
+  const goldPrice = await checkPrice(request)
+
+  request.ratio = 1
+  request.currency = 'EURTHB'
+  const euroPrice = await checkPrice(request)
+
+  request.ratio = 1
+  request.currency = 'USDTHB'
+  const usdPrice = await checkPrice(request)
+
+  request.ratio = 1
+  request.currency = 'JPYTHB'
+  const jpyPrice = await checkPrice(request)
+
+  const message: SlackMessageWithHeader = {
+    header: {
+      icon: UpwardsTrend,
+      messsage: `Price update`,
+    },
+    content: `${goldPrice}\n${euroPrice}\n${usdPrice}\n${jpyPrice}`,
+  }
+
+  const slackService = new SlackService()
+  await slackService.sendMessage(message)
+}
+
+const checkPrice = async (request: MarketDataRequest) => {
+  const marketDataService = new MarketDataService()
+
   const thaiBaht = Intl.NumberFormat('en-TH', {
     style: 'currency',
     currency: 'THB',
   })
   const result = await marketDataService.fetchCurrentPrice(request)
-  const goldPrice = thaiBaht.format(result)
-  const slackService = new SlackService()
-  const message: SlackMessageWithHeader = {
-    header: {
-      icon: UpwardsTrend,
-      messsage: 'Current gold price',
-    },
-    content: `The price of gold is now: ${goldPrice}.`,
-  }
-  await slackService.sendMessage(message)
+  const formattedResult = thaiBaht.format(result)
+  return `The price of ${request.currency} is now: ${formattedResult}.`
+
 }
