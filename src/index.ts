b679b040cc30dc74667436fe9fde9d162f547d75
Cédric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2024-02-23 22:19:15 +0700

    Remove APIs

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,9 @@
 import dotenv from 'dotenv'
-import express, { Express, Request, Response } from 'express'
+import * as main from './main'
+
 ;(async () => {
   dotenv.config()
-  const server: Express = express()
-  const port = process.env.PORT || 3000
-
-  server.get('/', (request: Request, response: Response) => {
-    response.json({ result: request.statusCode })
-  })
-
-  server.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-  });
+  await main.run()
 })().catch((e) => {
   console.error(e)
 })
