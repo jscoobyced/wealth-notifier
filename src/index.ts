cae24a01efb0d2092f7c833608db1057777d705c
Cédric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2024-02-23 21:48:05 +0700

    Start adding web-service

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import dotenv from 'dotenv'
-import * as Main from './main'
-
-(async () => {
+import express, { Express, Request, Response } from 'express'
+;(async () => {
   dotenv.config()
-  await Main.run()
-})().catch(e => {
+  const server: Express = express()
+  const port = process.env.PORT || 3000
+
+  server.get('/', (request: Request, response: Response) => {
+    response.json({ result: request.statusCode })
+  })
+})().catch((e) => {
   console.error(e)
 })
